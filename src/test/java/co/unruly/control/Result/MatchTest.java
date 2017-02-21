ceb532f25a16305b85fe7e74f519418113464bbe
Tom <tarjohnson1979@gmail.com>
2017-02-21 23:06:47 +0000

    Add tests for matching on value and arbitrary predicate

--- src/test/java/co/unruly/control/Result/MatchTest.java
+++ src/test/java/co/unruly/control/Result/MatchTest.java
@@ -4,16 +4,14 @@ import org.junit.Test;
 
 import java.util.function.Function;
 
-import static co.unruly.control.Result.Match.ifIs;
-import static co.unruly.control.Result.Match.ifType;
-import static co.unruly.control.Result.Match.match;
+import static co.unruly.control.Result.Match.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MatchTest {
 
     @Test
-    public void canMatchOnType() {
+    public void canMatchOnTypeWithFlowTyping() {
         Function<A, String> matchByType = match(
             ifType(B.class, B::messageForB),
             ifType(C.class, C::messageForC)
@@ -22,6 +20,32 @@ public class MatchTest {
         assertThat(matchByType.apply(new A("Cheese")), is("Cheese"));
         assertThat(matchByType.apply(new B("Ketchup")), is("I'm a B and I say Ketchup"));
         assertThat(matchByType.apply(new C("Pickles")), is("I'm a C and I say Pickles"));
+    }
+
+    @Test
+    public void canMatchOnValue() {
+        Function<Integer, String> matchByType = match(
+            ifEquals(4, x -> x + " sure looks like a 4 to me!"),
+            ifEquals(7, x -> x + " looks like one of them gosh-darned 7s?")
+        ).otherwise(x -> "I have no idea what a " + x + " is though...");
+
+        assertThat(matchByType.apply(3), is("I have no idea what a 3 is though..."));
+        assertThat(matchByType.apply(4), is("4 sure looks like a 4 to me!"));
+        assertThat(matchByType.apply(6), is("I have no idea what a 6 is though..."));
+        assertThat(matchByType.apply(7), is("7 looks like one of them gosh-darned 7s?"));
+    }
+
+    @Test
+    public void canMatchOnTest() {
+        Function<Integer, String> matchByType = match(
+            ifIs((Integer x) -> x % 2 == 0, x -> x + ", well, that's one of those even numbers"),
+            ifIs(x -> x < 0,                x -> x + " is one of those banker's negative number thingies")
+        ).otherwise(x -> x + " is a regular, god-fearing number for god-fearing folks");
+
+        assertThat(matchByType.apply(2), is("2, well, that's one of those even numbers"));
+        assertThat(matchByType.apply(-6), is("-6, well, that's one of those even numbers"));
+        assertThat(matchByType.apply(3), is("3 is a regular, god-fearing number for god-fearing folks"));
+        assertThat(matchByType.apply(-9), is("-9 is one of those banker's negative number thingies"));
     }
 
     static class A {
