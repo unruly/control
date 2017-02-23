42f59c79234a29d3ef353ea9a5e7335057b56ae8
Tom <tarjohnson1979@gmail.com>
2017-02-23 22:44:52 +0000

    Add a match statement which takes an initial value, for cases where we don't want to lift it as a function. Also introduce a case where we take optional providers and no input value.

--- src/main/java/co/unruly/control/Result/Match.java
+++ src/main/java/co/unruly/control/Result/Match.java
@@ -1,7 +1,11 @@
 package co.unruly.control.Result;
 
+import co.unruly.control.Unit;
+
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static co.unruly.control.Result.Casts.cast;
 import static co.unruly.control.Result.EndoAttempt.compose;
@@ -17,19 +21,29 @@ public class Match {
     }
 
     @SafeVarargs
+    public static <I, O> BoundMatchAttempt<I, O> matchValue(I inputValue, EndoAttempt<O, I>... potentialMatchers) {
+        return f -> attemptMatch(potentialMatchers).andFinally(orElseGet(f)).apply(inputValue);
+    }
+
+    @SafeVarargs
+    public static <O> BoundMatchSupplier<O> firstOf(EndoAttempt<O, Unit>... potentialMatchers) {
+        return s -> attemptMatch(potentialMatchers).andFinally(orElseGet(failure -> s.get())).apply(Unit.UNIT);
+    }
+
+    @SafeVarargs
     public static <S, F> Attempt<S, F, F, S> attemptMatch(EndoAttempt<F, S>... potentialMatches) {
         return Results.<S, F>invert().then(compose(potentialMatches));
     }
 
     public static <S, F, F1 extends F> EndoAttempt<S, F> ifType(Class<F1> type, Function<F1, S> function) {
         return x -> x.then(flatMapFailure(
-            failure -> cast(failure, type).then(flatMap(function.andThen(Result::success)))
+                failure -> cast(failure, type).then(flatMap(function.andThen(Result::success)))
         ));
     }
 
     public static <S, F> EndoAttempt<S, F> ifIs(Predicate<F> pred, Function<F, S> function) {
         return x -> x.then(flatMapFailure(
-            failure -> pred.test(failure) ? Result.success(function.apply(failure)) : Result.failure(failure)
+                failure -> pred.test(failure) ? Result.success(function.apply(failure)) : Result.failure(failure)
         ));
     }
 
@@ -37,8 +51,23 @@ public class Match {
         return ifIs(value::equals, function);
     }
 
+    public static <S, F> EndoAttempt<S, F> ifPresent(Supplier<Optional<S>> successProvider) {
+        return r -> r.then(Results.flatMapFailure(f -> successProvider.get().map(Result::<S, F>success).orElseGet(() -> Result.failure(f))));
+    }
+
+
     @FunctionalInterface
     public interface MatchAttempt<I, O> {
         Function<I, O> otherwise(Function<I, O> baseCase);
+    }
+
+    @FunctionalInterface
+    public interface BoundMatchAttempt<I, O> {
+        O otherwise(Function<I, O> baseCase);
+    }
+
+    @FunctionalInterface
+    public interface BoundMatchSupplier<O> {
+        O otherwise(Supplier<O> baseCase);
     }
 }
--- src/test/java/co/unruly/control/Result/MatchTest.java
+++ src/test/java/co/unruly/control/Result/MatchTest.java
@@ -2,6 +2,7 @@ package co.unruly.control.Result;
 
 import org.junit.Test;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import static co.unruly.control.Result.Match.*;
@@ -13,8 +14,8 @@ public class MatchTest {
     @Test
     public void canMatchOnTypeWithFlowTyping() {
         Function<A, String> matchByType = match(
-            ifType(B.class, B::messageForB),
-            ifType(C.class, C::messageForC)
+                ifType(B.class, B::messageForB),
+                ifType(C.class, C::messageForC)
         ).otherwise(A::message);
 
         assertThat(matchByType.apply(new A("Cheese")), is("Cheese"));
@@ -25,8 +26,8 @@ public class MatchTest {
     @Test
     public void canMatchOnValue() {
         Function<Integer, String> matchByType = match(
-            ifEquals(4, x -> x + " sure looks like a 4 to me!"),
-            ifEquals(7, x -> x + " looks like one of them gosh-darned 7s?")
+                ifEquals(4, x -> x + " sure looks like a 4 to me!"),
+                ifEquals(7, x -> x + " looks like one of them gosh-darned 7s?")
         ).otherwise(x -> "I have no idea what a " + x + " is though...");
 
         assertThat(matchByType.apply(3), is("I have no idea what a 3 is though..."));
@@ -38,14 +39,47 @@ public class MatchTest {
     @Test
     public void canMatchOnTest() {
         Function<Integer, String> matchByType = match(
-            ifIs((Integer x) -> x % 2 == 0, x -> x + ", well, that's one of those even numbers"),
-            ifIs(x -> x < 0,                x -> x + " is one of those banker's negative number thingies")
+                ifIs((Integer x) -> x % 2 == 0, x -> x + ", well, that's one of those even numbers"),
+                ifIs(x -> x < 0,                x -> x + " is one of those banker's negative number thingies")
         ).otherwise(x -> x + " is a regular, god-fearing number for god-fearing folks");
 
         assertThat(matchByType.apply(2), is("2, well, that's one of those even numbers"));
         assertThat(matchByType.apply(-6), is("-6, well, that's one of those even numbers"));
         assertThat(matchByType.apply(3), is("3 is a regular, god-fearing number for god-fearing folks"));
         assertThat(matchByType.apply(-9), is("-9 is one of those banker's negative number thingies"));
+    }
+
+    @Test
+    public void canMatchOnTestPassingArgument() {
+        String matchByResult = matchValue(4,
+                ifIs((Integer x) -> x % 2 == 0, x -> x + ", well, that's one of those even numbers"),
+                ifIs(x -> x < 0,                x -> x + " is one of those banker's negative number thingies")
+        ).otherwise(x -> x + " is a regular, god-fearing number for god-fearing folks");
+
+        assertThat(matchByResult, is("4, well, that's one of those even numbers"));
+    }
+
+    @Test
+    public void canOperateOverAListOfOptionalProviders() {
+        String cheese = firstOf(
+                ifPresent(() -> Optional.empty()),
+                ifPresent(() -> Optional.of("Cheese!")),
+                ifPresent(() -> Optional.of("Bacon!"))
+        ).otherwise(() -> "Ketchup!");
+
+        assertThat(cheese, is("Cheese!"));
+    }
+
+
+    @Test
+    public void usesDefaultIfNoOptionalProvidersProvideAValue() {
+        String cheese = firstOf(
+                ifPresent(() -> Optional.<String>empty()),
+                ifPresent(() -> Optional.empty()),
+                ifPresent(() -> Optional.empty())
+        ).otherwise(() -> "Ketchup!");
+
+        assertThat(cheese, is("Ketchup!"));
     }
 
     static class A {
