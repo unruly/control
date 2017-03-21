fec8e2ffcf521c3404682a21204f354ad118de10
Tom <tarjohnson1979@gmail.com>
2017-03-21 14:10:55 +0000

    Result no longer depends on ResultMapper

--- src/main/java/co/unruly/control/result/Result.java
+++ src/main/java/co/unruly/control/result/Result.java
@@ -95,7 +95,7 @@ public abstract class Result<S, F>  {
      * }
      * </pre>
      */
-    public <T, T2 extends T> T then(ResultMapper<S, F, T2> biMapper) {
+    public <T, T2 extends T> T then(Function<Result<S, F>, T2> biMapper) {
         return biMapper.apply(this);
     }
 
