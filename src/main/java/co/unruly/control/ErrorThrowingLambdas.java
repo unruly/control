01cf113970bd9c99f879b61ca74e891de364094f
tom <tarjohnson1979@gmail.com>
2017-08-13 10:38:48 +0100

    Add ErrorThrowingLambdas to express lambdas which throw any sort of Throwable, not just Exceptions

--- src/main/java/co/unruly/control/ErrorThrowingLambdas.java
+++ src/main/java/co/unruly/control/ErrorThrowingLambdas.java
@@ -7,16 +7,20 @@ import java.util.function.Predicate;
 
 /**
  * A collection of functional interfaces which throw, and convenience functions to wrap them
- * so any thrown exceptions are converted to RuntimeExceptions so they can be used where
+ * so any thrown Throwables are converted to RuntimeExceptions so they can be used where
  * non-throwing functional interfaces are required
+ *
+ * Catching errors in the general case is not recommended, but there are specific errors
+ * which are contextually reasonable to catch. Therefore, this wider capability exists
+ * separately and should be used judiciously.
  */
-public interface ThrowingLambdas {
+public interface ErrorThrowingLambdas {
 
     /**
      * A Function which may throw a checked exception
      */
     @FunctionalInterface
-    interface ThrowingFunction<I, O, X extends Exception> {
+    interface ThrowingFunction<I, O, X extends Throwable> {
         O apply(I input) throws X;
 
         default <T> ThrowingFunction<I, T, X> andThen(Function<O, T> nextFunction) {
@@ -31,11 +35,11 @@ public interface ThrowingLambdas {
          * Converts the provided function into a regular Function, where any thrown exceptions are
          * wrapped in a RuntimeException.
          */
-        static <I, O, X extends Exception> Function<I, O> throwingRuntime(ThrowingFunction<I, O, X> f) {
+        static <I, O, X extends Throwable> Function<I, O> throwingRuntime(ThrowingFunction<I, O, X> f) {
             return x -> {
                 try {
                     return f.apply(x);
-                } catch (Exception ex) {
+                } catch (Throwable ex) {
                     throw new RuntimeException(ex);
                 }
             };
@@ -46,18 +50,18 @@ public interface ThrowingLambdas {
      * A Consumer which may throw a checked exception
      */
     @FunctionalInterface
-    interface ThrowingConsumer<T, X extends Exception> {
+    interface ThrowingConsumer<T, X extends Throwable> {
         void accept(T item) throws X;
 
         /**
          * Converts the provided consumer into a regular Consumer, where any thrown exceptions are
          * wrapped in a RuntimeException.
          */
-        static <T, X extends Exception> Consumer<T> throwingRuntime(ThrowingConsumer<T, X> p) {
+        static <T, X extends Throwable> Consumer<T> throwingRuntime(ThrowingConsumer<T, X> p) {
             return x -> {
                 try {
                     p.accept(x);
-                } catch (Exception ex) {
+                } catch (Throwable ex) {
                     throw new RuntimeException(ex);
                 }
             };
@@ -68,18 +72,18 @@ public interface ThrowingLambdas {
      * A BiFunction which may throw a checked exception
      */
     @FunctionalInterface
-    interface ThrowingBiFunction<A, B, R, X extends Exception> {
+    interface ThrowingBiFunction<A, B, R, X extends Throwable> {
         R apply(A first, B second) throws X;
 
         /**
          * Converts the provided bifunction into a regular BiFunction, where any thrown exceptions
          * are wrapped in a RuntimeException
          */
-        static <A, B, R, X extends Exception> BiFunction<A, B, R> throwingRuntime(ThrowingBiFunction<A, B, R, X> f) {
+        static <A, B, R, X extends Throwable> BiFunction<A, B, R> throwingRuntime(ThrowingBiFunction<A, B, R, X> f) {
             return (a, b) -> {
                 try {
                     return f.apply(a, b);
-                } catch (Exception ex) {
+                } catch (Throwable ex) {
                     throw new RuntimeException(ex);
                 }
             };
@@ -90,18 +94,18 @@ public interface ThrowingLambdas {
      * A Predicate which may throw a checked exception
      */
     @FunctionalInterface
-    interface ThrowingPredicate<T, X extends Exception> {
+    interface ThrowingPredicate<T, X extends Throwable> {
         boolean test(T item) throws X;
 
         /**
          * Converts the provided predicate into a regular Predicate, where any thrown exceptions
          * are wrapped in a RuntimeException
          */
-        static <T, X extends Exception> Predicate<T> throwingRuntime(ThrowingPredicate<T, X> p) {
+        static <T, X extends Throwable> Predicate<T> throwingRuntime(ThrowingPredicate<T, X> p) {
             return x -> {
                 try {
                     return p.test(x);
-                } catch (Exception ex) {
+                } catch (Throwable ex) {
                     throw new RuntimeException(ex);
                 }
             };
